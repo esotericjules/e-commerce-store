a67f515a2560c27b7d16c9974fc817862378aaf6
Juliet Onyekaoha <juliet.onyekaoha@venturegargengroup.com>
2020-09-09 07:33:36 +0100

    created cart-icon component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,6 +4,7 @@ import "./header.styles.scss";
 import {Link} from "react-router-dom";
 import {ReactComponent as Logo} from "../../assets/crown.svg"
 import {auth} from "../../firebase/firebase.utils";
+import CartIcon from "../cart-icon/cart-icon.component";
 
 const Header = ({currentUser}) => {
   return (
@@ -29,6 +30,7 @@ const Header = ({currentUser}) => {
               SIGN IN
             </Link>
         }
+        <CartIcon/>
       </div>
     </div>
   );
