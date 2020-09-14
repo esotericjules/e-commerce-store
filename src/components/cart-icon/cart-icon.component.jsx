ef693b58a01f5c1faf23e9b0e5d6e81012cea4a6
Juliet Onyekaoha <juliet.onyekaoha@venturegargengroup.com>
2020-09-14 07:17:42 +0100

    added checkout page, checkout item component and updated cart to use selectors from reselect

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import {connect} from "react-redux";
+import {createStructuredSelector} from 'reselect';
+
 import {toggleCartHidden} from "../../redux/cart/cart.actions";
 import {ReactComponent as ShoppingIcon} from "../../assets/shopping-bag.svg";
 import "./cart-icon.styles.scss";
@@ -19,8 +21,8 @@ const mapDispatchToProps = dispatch => ({
 });
 
 
-const mapStateToProps = (state) => ({
-  itemCount: selectCartItemsCount(state)
+const mapStateToProps = createStructuredSelector({
+  itemCount: selectCartItemsCount
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,10 +2,14 @@ import React from 'react';
 import {connect} from "react-redux";
 import "./header.styles.scss";
 import {Link} from "react-router-dom";
+import {createStructuredSelector} from 'reselect';
+
 import {ReactComponent as Logo} from "../../assets/crown.svg"
 import {auth} from "../../firebase/firebase.utils";
 import CartIcon from "../cart-icon/cart-icon.component";
 import CartDropdown from "../cart-dropdown/cart-dropdown.component";
+import {selectCartHidden} from "../../redux/cart/cart.selectors";
+import {selectCurrentUser} from "../../redux/user/user.selector";
 
 const Header = ({currentUser, hidden}) => {
   return (
@@ -42,9 +46,9 @@ const Header = ({currentUser, hidden}) => {
   );
 };
 
-const mapStateToProps = ({user: {currentUser}, cart: {hidden}}) => ({
-  currentUser,
-  hidden
+const mapStateToProps = createStructuredSelector({
+  currentUse: selectCurrentUser,
+  hidden: selectCartHidden,
 });
 
 export default connect(mapStateToProps)(Header);
